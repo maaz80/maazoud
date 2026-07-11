1b616d888fea3419ac72ce038722f97ad05e34bf
maaz80 <maazforlap@gmail.com>
2026-07-11 22:46:20 +0530

    Added loading

---
+++ app/loading.js
@@ -0,0 +1,7 @@
+export default function Loading() {
+  return (
+    <div className="flex items-center justify-center min-h-[50vh]">
+      <div className="w-12 h-12 border-4 border-stone-200 border-t-amber-600 rounded-full animate-spin"></div>
+    </div>
+  );
+}
